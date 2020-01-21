18b2e6ac9f08ffe434b6c074dd94be1d52772e22
MikhailArkhipov <mikhaila@microsoft.com>
2020-01-21 15:31:31 -0800

    Add PyRx settings

--- server/server.ts
+++ server/server.ts
@@ -8,13 +8,44 @@
 import * as assert from 'assert';
 import * as fs from 'fs';
 import * as path from 'path';
-import { LanguageServerBase } from './pyright/server/src/languageServerBase';
+import { LanguageServerBase, WorkspaceServiceInstance, ServerSettings } from './pyright/server/src/languageServerBase';
+import { isArray } from 'util';
 
 class Server extends LanguageServerBase {
     constructor() {
         assert(fs.existsSync(path.join(__dirname, 'typeshed-fallback')), 'Unable to locate typeshed fallback folder.');
         super('PyRx', __dirname);
         this.console.log(`PyRx server root directory: ${__dirname}`);
+    }
+
+    async getSettings(workspace: WorkspaceServiceInstance): Promise<ServerSettings> {
+        const serverSettings: ServerSettings = {
+            disableLanguageServices: false
+        };
+
+        try {
+            const pythonSection = await this.getConfiguration(workspace, 'python');
+            if (pythonSection) {
+                serverSettings.pythonPath = pythonSection['pythonPath'];
+                serverSettings.venvPath = pythonSection['venvPath'];
+            }
+            
+            const pythonAnalysisSection = await this.getConfiguration(workspace, 'python.analysis');
+            if (pythonAnalysisSection) {
+                const typeshedPaths = pythonAnalysisSection['typeshedPaths'];
+                if (typeshedPaths && isArray(typeshedPaths) && typeshedPaths.length > 0) {
+                    serverSettings.typeshedPath = typeshedPaths[0];
+                }
+                serverSettings.openFilesOnly = !!pythonAnalysisSection['openFilesOnly'];
+                serverSettings.useLibraryCodeForTypes = !!pythonAnalysisSection['useLibraryCodeForTypes'];
+            } else {
+                serverSettings.openFilesOnly = true;
+                serverSettings.useLibraryCodeForTypes = true;
+            }
+        } catch (error) {
+            this.console.log(`Error reading settings: ${error}`);
+        }
+        return serverSettings;
     }
 }
 
