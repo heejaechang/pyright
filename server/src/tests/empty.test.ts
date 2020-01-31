747121d3bce41593d6190ab2151defce84fb00c0
HeeJae Chang <hechang@microsoft.com>
2020-01-31 15:35:01 -0800

    added npm run test:all that will run both pyrx and pyright tests in pyrx/server package.json

---
+++ server/src/tests/empty.test.ts
@@ -0,0 +1,13 @@
+/*
+* empty.test.ts
+* Copyright (c) Microsoft Corporation.
+* Licensed under the MIT license.
+*
+* place holder
+*/
+
+import * as assert from 'assert';
+
+test('place holder', () => {
+    assert(true);
+});
