40cee9f2f8505a4cb2ebf57e1d0ab7b619e0d1c1
Jake Bailey <5341706+jakebailey@users.noreply.github.com>
2020-02-12 15:48:53 -0800

    Add starter stubs for matplotlib.pyplot and a few other bits (#93)
    * Add starter stubs for matplotlib.pyplot and a few other bits
    
    * Shift Axes around to match where it actually lives
    
    * Revert movement, start a little of Axes

---
+++ server/bundled-stubs/matplotlib/_typing.pyi
@@ -0,0 +1,31 @@
+# Internally defined types, which should be moved elsewhere.
+
+# This should come from numpy.
+from typing import Any, List, NewType, Sequence, Union
+
+
+ndarray = NewType("ndarray", object)
+
+# TODO: This should come from numpy, whatever np.isscalar() accepts.
+Scalar = Union[
+    int,
+    float,
+    bool,
+    complex,
+]
+# Scalar = Any
+
+
+# TODO: This should come from numpy, whatever np.array() accepts.
+ArrayLike = Union[
+    ndarray,
+    List[Any],
+    Sequence[Scalar],
+    Scalar, # TODO: Should this be here?
+]
+# ArrayLike = Any
+
+
+# TODO: type PIL
+PIL_Image = NewType("PIL_IMAGE", object)
+# PIL_Image = Any
