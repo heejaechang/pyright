e30de09886615091ac3bc345ec3a02b1653101f9
Jake Bailey <5341706+jakebailey@users.noreply.github.com>
2020-02-21 17:15:22 -0800

    Remove pyright temporarily again

--- server/pyright/server/src/tests/samples/assert1.py
+++
@@ -1,6 +0,0 @@
-# This sample tests the ability to detect errant assert calls
-# that are always true - the "reportAssertAlwaysTrue" option.
-
-# This should generate a warning
-assert (1 != 2, "Error message")
-
