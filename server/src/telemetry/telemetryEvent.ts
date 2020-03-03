e38476d78ab23f1853c85335befc10a24adba679
Bill Schnurr <bschnurr@microsoft.com>
2020-03-03 10:42:54 -0800

    Bschnurr/add analysis telemetry (#117)
    * adding analysis completed telemtry to track memory usage and duration of batch file processing
    
    * add analysis completed event with memory tracking and num files we processed
    
    * adding back spacing
    
    * fixing delegate crash but using bind
    adding try/finally and changing how telemetry event is created
    
    * trying to make telemetryEvent a type
    
    * adding more specific types to telemetryEvent
    
    * removing delegate and connection from analysisTracker.  Now we return a telemetryEvent instead
    
    * removing TelemetryLogEventDelegate. no longer used
    
    * switching back to early exit style
    
    * making telemetryEvent properties readonly to prevent stomping
    
    * set telemetry version to unknow
    
    * changing callback binding to use fat arrow to maintain type info of "this"
    
    https://github.com/Microsoft/TypeScript/wiki/%27this%27-in-TypeScript#fixes
    
    * rename _numFilesAnalysed ->_numFilesAnalyzed
    
    * adding note about telemetry names matching extension
    
    * Limiting analysis telemetry to wait at least 60 seconds
    
    * fixing typos and adding fileInProgram count
    
    * renaming telemetry filesInProgram to numFilesInProgram
    
    Co-authored-by: Jake Bailey <5341706+jakebailey@users.noreply.github.com>

---
+++ server/src/telemetry/telemetryEvent.ts
@@ -0,0 +1,31 @@
+/*
+ * telemetry.ts
+ * Copyright (c) Microsoft Corporation.
+ * Licensed under the MIT license.
+ */
+
+const TelemetryPrefix = 'mplsv2/';
+
+// Note: These names must match the expected values in the VSCode Python Extension
+// https://github.com/microsoft/vscode-python/blob/master/src/client/activation/languageServer/languageServerProxy.ts
+export interface TelemetryEvent {
+    readonly EventName: string;
+
+    readonly Properties: {
+        [key: string]: string;
+    };
+
+    readonly Measurements: {
+        [key: string]: number;
+    };
+}
+
+export function createTelemetryEvent(eventName: string): TelemetryEvent {
+    return {
+        EventName: TelemetryPrefix + eventName,
+        Properties: {
+            Version: 'unknown'
+        },
+        Measurements: {}
+    };
+}
