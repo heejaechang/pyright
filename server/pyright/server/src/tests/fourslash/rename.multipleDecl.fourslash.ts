99a29c473a54526d877062120e89e98608086fdb
Hugues Valois <huvalo@microsoft.com>
2020-05-28 18:19:06 -0700

    Update rename test.

--- server/pyright/server/src/tests/fourslash/rename.multipleDecl.fourslash.ts
+++ server/pyright/server/src/tests/fourslash/rename.multipleDecl.fourslash.ts
@@ -1,25 +1,21 @@
 /// <reference path="fourslash.ts" />
 
-// @filename: mspythonconfig.json
-//// {
-////   "useLibraryCodeForTypes": true
-//// }
-
 // @filename: foo/__init__.py
-// @library: true
 //// class Foo:
 ////    pass
 
 // @filename: test.py
 //// import foo
-//// import os as foo
 //// [|/*marker*/foo|] = 3
-//// def foo(): pass
+//// def [|foo|](): pass
 
-// one of the declarations is from site-packages, so do not rename
+const ranges = helper.getRanges();
+
 helper.verifyRename({
     marker: {
         newName: 'foo1',
-        changes: [],
+        changes: ranges.map((r) => {
+            return { filePath: r.fileName, range: helper.convertPositionRange(r), replacementText: 'foo1' };
+        }),
     },
 });
