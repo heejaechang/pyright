d6b01f745ab5d997a9c48d985c89c5e1f901b00f
Jake Bailey <5341706+jakebailey@users.noreply.github.com>
2020-10-26 15:20:46 -0700

    git subrepo pull (merge) packages/pyright
    subrepo:
      subdir:   "packages/pyright"
      merged:   "c7881e33"
    upstream:
      origin:   "https://github.com/microsoft/pyright.git"
      branch:   "master"
      commit:   "22c6806c"
    git-subrepo:
      version:  "0.4.1"
      origin:   "???"
      commit:   "???"

--- packages/pyright/packages/pyright-internal/src/tests/typeEvaluator2.test.ts
+++ packages/pyright/packages/pyright-internal/src/tests/typeEvaluator2.test.ts
@@ -469,6 +469,12 @@ test('Protocol9', () => {
     TestUtils.validateResults(analysisResults, 0);
 });
 
+test('Protocol10', () => {
+    const analysisResults = TestUtils.typeAnalyzeSampleFiles(['protocol10.py']);
+
+    TestUtils.validateResults(analysisResults, 0);
+});
+
 test('TypedDict1', () => {
     const analysisResults = TestUtils.typeAnalyzeSampleFiles(['typedDict1.py']);
 
@@ -871,6 +877,20 @@ test('Unions2', () => {
     configOptions.defaultPythonVersion = PythonVersion.V3_8;
     const analysisResults38 = TestUtils.typeAnalyzeSampleFiles(['unions2.py'], configOptions);
     TestUtils.validateResults(analysisResults38, 0);
+});
+
+test('Unions3', () => {
+    const configOptions = new ConfigOptions('.');
+
+    // Analyze with Python 3.9 settings. This will generate errors.
+    configOptions.defaultPythonVersion = PythonVersion.V3_9;
+    const analysisResults3_9 = TestUtils.typeAnalyzeSampleFiles(['unions3.py'], configOptions);
+    TestUtils.validateResults(analysisResults3_9, 1);
+
+    // Analyze with Python 3.10 settings.
+    configOptions.defaultPythonVersion = PythonVersion.V3_10;
+    const analysisResults3_10 = TestUtils.typeAnalyzeSampleFiles(['unions3.py'], configOptions);
+    TestUtils.validateResults(analysisResults3_10, 0);
 });
 
 test('ParamSpec1', () => {
