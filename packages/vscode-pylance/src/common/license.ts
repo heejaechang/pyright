6befca3da3401869ae56b291f6e444a88a04bfbb
Jake Bailey <5341706+jakebailey@users.noreply.github.com>
2020-10-27 12:39:12 -0700

    Rewrite license error in terms of our license (#606)

--- packages/vscode-pylance/src/common/license.ts
+++ packages/vscode-pylance/src/common/license.ts
@@ -1,12 +1,16 @@
+// Select lines pulled from LICENSE.txt.
+// This string is chunked to give the obfuscator more chances to split it up (and to make it a bit easier to read).
 export const licenseErrorText = [
-    'You may only use the Pylance extension with Visual Studio Code, Visual Studio or Xamarin Studio software',
-    'to help you develop and test your applications.',
-    'The software is licensed, not sold.',
-    'This agreement only gives you some rights to use the software.',
-    'Microsoft reserves all other rights',
-    'You may not work around any technical limitations in the software;',
-    'reverse engineer, decompile or disassemble the software',
-    'remove, minimize, block or modify any notices of Microsoft or ',
-    'its suppliers in the software share, publish, rent, or lease ',
-    'the software, or provide the software as a stand-alone hosted as solution for others to use.',
-].join('\n');
+    'You may install and use any number of copies of the software only with Microsoft Visual Studio,',
+    'Visual Studio for Mac, Visual Studio Code, Azure DevOps, Team Foundation Server, and successor Microsoft products',
+    'and services (collectively, the “Visual Studio Products and Services”) to develop and test your applications.',
+    'The software is licensed, not sold. This agreement only gives you some rights to use the software. Microsoft reserves all other rights.',
+    'You may not:',
+    'work around any technical limitations in the software that only allow you to use it in certain ways;',
+    'reverse engineer, decompile or disassemble the software, or otherwise attempt to derive the source code for the software,',
+    'except and to the extent required by third party licensing terms governing use of certain open source components that may be included in the software;',
+    'remove, minimize, block, or modify any notices of Microsoft or its suppliers in the software;',
+    'use the software in any way that is against the law or to create or propagate malware; or',
+    'share, publish, distribute, or lease the software (except for any distributable code, subject to the terms above),',
+    'provide the software as a stand-alone offering for others to use, or transfer the software or this agreement to any third party.',
+].join(' ');
