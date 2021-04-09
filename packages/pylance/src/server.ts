1db9f73347032c8a91a525dfd79735c83c693aa3
Jake Bailey <5341706+jakebailey@users.noreply.github.com>
2021-04-09 10:31:42 -0700

    Add flag to print version and exit to dedicated pylance package (#1319)

--- packages/pylance/src/server.ts
+++ packages/pylance/src/server.ts
@@ -3,6 +3,12 @@ if (process.env.NODE_ENV === 'production') {
     (process as any).noDeprecation = true;
 }
 
+import { VERSION } from 'pylance-internal/common/constants';
 import { main } from 'pylance-internal/server';
+
+if (process.argv.some((arg) => arg === '--version')) {
+    console.log(`pylance ${VERSION}`);
+    process.exit(0);
+}
 
 main();
