89a76be77043ceb5826bc0afff20d0f1329cd759
HeeJae Chang <hechang@microsoft.com>
2021-09-30 14:18:50 -0700

    added more tests

--- packages/pyright/packages/pyright-internal/src/tests/fourslash/completions.parentFolders.fourslash.ts
+++ packages/pyright/packages/pyright-internal/src/tests/fourslash/completions.parentFolders.fourslash.ts
@@ -18,6 +18,15 @@
 // @filename: nested1/nested2/test2.py
 //// from ..[|/*marker2*/|]
 
+// @filename: nested1/nested2/test3.py
+//// from ..nested2.[|/*marker3*/|]
+
+// @filename: nested1/nested2/test4.py
+//// from ...nested1.[|/*marker4*/|]
+
+// @filename: nested1/nested2/test5.py
+//// from ...nested1.nested2.[|/*marker5*/|]
+
 {
     helper.openFiles(helper.getMarkers().map((m) => m.fileName));
 
@@ -28,6 +37,9 @@
                 { label: 'import', kind: Consts.CompletionItemKind.Keyword },
                 { label: 'test1', kind: Consts.CompletionItemKind.Module },
                 { label: 'test2', kind: Consts.CompletionItemKind.Module },
+                { label: 'test3', kind: Consts.CompletionItemKind.Module },
+                { label: 'test4', kind: Consts.CompletionItemKind.Module },
+                { label: 'test5', kind: Consts.CompletionItemKind.Module },
             ],
         },
         marker2: {
@@ -35,6 +47,30 @@
                 { label: 'import', kind: Consts.CompletionItemKind.Keyword },
                 { label: 'nested2', kind: Consts.CompletionItemKind.Module },
                 { label: 'module', kind: Consts.CompletionItemKind.Module },
+            ],
+        },
+        marker3: {
+            completions: [
+                { label: 'test1', kind: Consts.CompletionItemKind.Module },
+                { label: 'test2', kind: Consts.CompletionItemKind.Module },
+                { label: 'test3', kind: Consts.CompletionItemKind.Module },
+                { label: 'test4', kind: Consts.CompletionItemKind.Module },
+                { label: 'test5', kind: Consts.CompletionItemKind.Module },
+            ],
+        },
+        marker4: {
+            completions: [
+                { label: 'nested2', kind: Consts.CompletionItemKind.Module },
+                { label: 'module', kind: Consts.CompletionItemKind.Module },
+            ],
+        },
+        marker5: {
+            completions: [
+                { label: 'test1', kind: Consts.CompletionItemKind.Module },
+                { label: 'test2', kind: Consts.CompletionItemKind.Module },
+                { label: 'test3', kind: Consts.CompletionItemKind.Module },
+                { label: 'test4', kind: Consts.CompletionItemKind.Module },
+                { label: 'test5', kind: Consts.CompletionItemKind.Module },
             ],
         },
     });
